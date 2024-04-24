aaf0857354d57fb6c27eb64e433546be88576a72
AM0312 <ansh.midha0312@gmail.com>
2024-04-24 15:22:13 +0530

    Added header file

---
+++ Flappy Bird/flappyHeader.h
@@ -0,0 +1,14 @@
+#pragma once
+extern double queuedMilliseconds, prev0, fps, responseTime;
+extern int resX, resY, movementX, insidebrick, gameover, startFlag, god;
+extern double movementY;
+
+void adjustBrick();
+void draw();
+void resetFunc();
+void gameoverAnimation();
+void mixedStepLoop();
+void update(double);
+void updateSynchronised();
+void processSpecialKeys(int, int, int);
+void movePhysics();
