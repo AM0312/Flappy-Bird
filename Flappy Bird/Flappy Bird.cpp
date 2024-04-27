e69f65128f61b190c5368ecd8abd51bdebac5494
AM0312 <ansh.midha0312@gmail.com>
2024-04-27 17:12:06 +0530

    reshape callback

--- Flappy Bird/Flappy Bird.cpp
+++ Flappy Bird/Flappy Bird.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <gl/glut.h>
+#include <vector>
+#include <cmath>
+#include <string>
+#include "flappyHeader.h"
 #include"../SOIL2/SOIL2.h"
+using namespace std;
+
+int resX, resY;
+
+//... Reshaping setting & rendering
+void reshape(int w, int h) {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glOrtho(0, resX, 0, resY, -5, 10);
+	glLoadIdentity();
+}
