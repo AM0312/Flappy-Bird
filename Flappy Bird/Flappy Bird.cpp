27ca5dec68aa870d543906085ec386e476868e06
AM0312 <ansh.midha0312@gmail.com>
2024-04-27 17:24:15 +0530

    physics is good

--- Flappy Bird/Flappy Bird.cpp
+++ Flappy Bird/Flappy Bird.cpp
@@ -7,7 +7,12 @@
 #include"../SOIL2/SOIL2.h"
 using namespace std;
 
-int resX, resY;
+int resX, resY, movX, startFlag;
+double movY;
+
+vector<char> inputKeys;
+double v, grav, temp2;
+int cnt;
 
 //... Reshaping setting & rendering
 void reshape(int w, int h) {
@@ -16,4 +21,44 @@ void reshape(int w, int h) {
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 	glOrtho(0, resX, 0, resY, -5, 10);
 	glLoadIdentity();
+}
+
+void gameOver() {
+	inputKeys.push_back('U');
+	v = 1.2;
+}
+
+void movPhysics() {
+	grav = 10;
+	for (int _ = 0; _ < inputKeys.size(); _++) {
+		if (inputKeys[_] == 'U') {
+			if ((movY + (resY / 2) + 50) < resY) movY += 4;
+			cnt++;
+		}
+		if (cnt >= 10) {
+			cnt = 0;
+			inputKeys.erase(inputKeys.begin() + _);
+		}
+	}
+	if (startFlag) {
+		//... Gravity exists
+		if (inputKeys.size() == 0) {
+			if (gameover == 0) {
+				temp2 = resY / 2;
+				v = 0.65 + (abs(movY) / temp2) / 5;
+			}
+			else {
+				if ((inputKeys.size() == 0) and (movY < -(resY / 2))) {
+					gameover = 0;
+					resetFunc();
+				}
+			}
+			if ((movY < (-resY / 2 - 30))) {
+				if (!god) movY -= v * v / 2 * grav;
+			}
+			else {
+				movY -= v * v / 2 * grav;
+			}
+		}
+	}
 }
